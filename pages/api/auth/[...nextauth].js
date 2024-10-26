0eae321f1dc07412417adbb0099bd65e2c5f6284
Emaad <emadfredev@gmail.com>
2024-10-26 12:30:33 +0330

    created a page for login in client side and developed api for it

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,40 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import User from "../../../models/User";
+import { verifyPassword } from "../../../utils/auth";
 
-const authOptions ={
+const authOptions = {
     session: { strategy: "jwt" },
     providers: [
         CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "Enter your email" },
-                password: { label: "Password", type: "password", placeholder: "Enter your password" }
-            },
+            // This code is used for creating form in client side
+            // name: "Credentials",
+            // credentials: {
+            //     email: { label: "Email", type: "email", placeholder: "Enter your email" },
+            //     password: { label: "Password", type: "password", placeholder: "Enter your password" }
+            // },
+
             async authorize(credentials, req) {
-                return { name: "Milad" }
+                const { email, password } = credentials;
+                try {
+                    await connectDB();
+                } catch (error) {
+                    throw new Error("Error in connecting to DB")
+                }
+                if(!email || !password) {
+                    throw new Error("Email and password are required");
+                }
+
+                const user = await User.findOne({email});
+                if(!email) {
+                    throw new Error("User not found");
+                }
+
+                const isValid = await verifyPassword(password, user.password);
+                if(!isValid) {
+                    throw new Error("Username or password is incorrect");
+                }
+                return {email};
             }
         })
     ]
--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,29 @@
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+
 const SignIn = () => {
-    return <h1>Sign In</h1>
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignIn = async () => {
+        const res = await signIn("credentials", {
+            email,
+            password,
+            redirect: false
+        })
+        console.log(res);
+    }
+
+
+    return <>
+        <h1>Sign In</h1>
+        <form>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+            <button onClick={handleSignIn}>Sign In</button>
+        </form>
+    </>
 }
 
 export default SignIn;
